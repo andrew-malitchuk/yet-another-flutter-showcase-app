87b554435ea7e0c2e2cc6f9b1d5e03f322c9dd41
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-02-14 20:50:06 +0200

    feat: add database_configure

---
+++ lib/yafsa/data/database/core/configure/database_configure.dart
@@ -0,0 +1,9 @@
+class DatabaseModel {
+  static const book = 0;
+  static const author = 1;
+}
+
+class DatabaseConfigure {
+  static const bookBox = 'bookBox';
+  static const libraryBox = 'libraryBox';
+}
