0f63f4b7cc5f01e19c6394d3b9dae98c9b1ccc9a
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-03-11 21:08:01 +0200

    feat: setup navigation

---
+++ lib/yafsa/presentation/core/navigation/core/transition/trsnsition_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+
+CustomTransitionPage buildCustomTransitionPage({
+  required BuildContext context,
+  required GoRouterState state,
+  required Widget child,
+}) {
+  return CustomTransitionPage(
+    key: state.pageKey,
+    child: child,
+    transitionDuration: const Duration(milliseconds: 1000),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return FadeTransition(
+        opacity: CurveTween(curve: Curves.easeInOutCirc).animate(animation),
+        child: child,
+      );
+    },
+  );
+}
