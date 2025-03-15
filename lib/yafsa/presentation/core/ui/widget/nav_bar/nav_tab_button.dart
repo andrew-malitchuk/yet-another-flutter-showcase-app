5c90cdb62a6ded0382d73146c92f780355f8af7a
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-03-15 21:10:09 +0200

    feat: add navigation_bar

---
+++ lib/yafsa/presentation/core/ui/widget/nav_bar/nav_tab_button.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show HapticFeedback;
+
+import 'nav_button.dart';
+import 'nav_style.dart';
+
+class TabButton extends StatefulWidget {
+  final bool? active;
+  final bool? debug;
+  final bool? haptic;
+  final double? gap;
+  final Color? iconColor;
+  final Color? rippleColor;
+  final Color? hoverColor;
+  final Color? iconActiveColor;
+  final Color? textColor;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+  final TextStyle? textStyle;
+  final double? iconSize;
+  final Function? onPressed;
+  final String text;
+  final String icon;
+  final Color? backgroundColor;
+  final Duration? duration;
+  final Curve? curve;
+  final Gradient? backgroundGradient;
+  final Widget? leading;
+  final BorderRadius? borderRadius;
+  final Border? border;
+  final Border? activeBorder;
+  final List<BoxShadow>? shadow;
+  final String? semanticLabel;
+  final NavStyle? style;
+  final double? textSize;
+
+  const TabButton({
+    super.key,
+    this.active,
+    this.haptic,
+    this.backgroundColor,
+    required this.icon,
+    this.iconColor,
+    this.rippleColor,
+    this.hoverColor,
+    this.iconActiveColor,
+    this.text = '',
+    this.textColor,
+    this.padding,
+    this.margin,
+    this.duration,
+    this.debug,
+    this.gap,
+    this.curve,
+    this.textStyle,
+    this.iconSize,
+    this.leading,
+    this.onPressed,
+    this.backgroundGradient,
+    this.borderRadius,
+    this.border,
+    this.activeBorder,
+    this.shadow,
+    this.semanticLabel,
+    this.style = NavStyle.google,
+    this.textSize,
+  });
+
+  @override
+  _TabButtonState createState() => _TabButtonState();
+}
+
+class _TabButtonState extends State<TabButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      label: widget.semanticLabel ?? widget.text,
+      child: NavButton(
+        textSize: widget.textSize,
+        style: widget.style,
+        borderRadius: widget.borderRadius,
+        border: widget.border,
+        activeBorder: widget.activeBorder,
+        shadow: widget.shadow,
+        debug: widget.debug,
+        duration: widget.duration,
+        iconSize: widget.iconSize,
+        active: widget.active,
+        onPressed: () {
+          if (widget.haptic!) HapticFeedback.selectionClick();
+          widget.onPressed?.call();
+        },
+        padding: widget.padding,
+        margin: widget.margin,
+        gap: widget.gap,
+        color: widget.backgroundColor,
+        rippleColor: widget.rippleColor,
+        hoverColor: widget.hoverColor,
+        gradient: widget.backgroundGradient,
+        curve: widget.curve,
+        leading: widget.leading,
+        iconActiveColor: widget.iconActiveColor,
+        iconColor: widget.iconColor,
+        icon: widget.icon,
+        text: Text(widget.text,
+            style: widget.textStyle?.copyWith(
+              color: widget.textColor,
+            )),
+      ),
+    );
+  }
+}
