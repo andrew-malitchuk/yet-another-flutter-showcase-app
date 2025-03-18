b1c54106079614f24628bbdd540ff1a51afa5537
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-03-18 21:11:07 +0200

    feat: add root

---
+++ lib/yafsa/presentation/feature/root/root_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:yafsa/yafsa/presentation/feature/root/root_view.dart';
+
+class RootPage extends StatelessWidget {
+  const RootPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RootView();
+  }
+}
