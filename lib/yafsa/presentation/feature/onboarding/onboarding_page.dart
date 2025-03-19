aaa7eabe47c525876aae68a7dc56eab8177767ba
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-03-19 21:11:45 +0200

    feat: add onboarding

---
+++ lib/yafsa/presentation/feature/onboarding/onboarding_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+import 'onboarding_view.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return OnboardingView();
+  }
+}
