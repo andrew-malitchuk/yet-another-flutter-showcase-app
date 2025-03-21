7be79e5f25007233a573503ef7493e685bd41192
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-03-21 21:13:20 +0200

    feat: add home layout

---
+++ lib/yafsa/presentation/feature/main/home/core/components/layout/layout_loading.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class LoadingLayout extends StatelessWidget {
+  const LoadingLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+    final textTheme = theme.textTheme;
+
+    return Shimmer.fromColors(
+      baseColor: colorScheme.onSurface,
+      highlightColor: colorScheme.surface,
+      child: ListView.builder(
+        itemCount: 3, // Adjust the count based on your needs
+        itemBuilder: (context, index) {
+          return Padding(
+              padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+              child: ShimmerBookItem());
+        },
+      ),
+    );
+  }
+}
+
+class ShimmerBookItem extends StatelessWidget {
+  const ShimmerBookItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 150,
+        child: Row(
+          children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Container(
+                  height: 150,
+                  width: 100,
+                  color: Colors.white,
+                )),
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(left: 8),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Container(
+                          height: 16,
+                          width: 200,
+                          color: Colors.white,
+                        ))),
+                Padding(
+                    padding: EdgeInsets.only(left: 8, top: 8),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Container(
+                          height: 16,
+                          width: 100,
+                          color: Colors.white,
+                        ))),
+              ],
+            ))
+          ],
+        ));
+  }
+}
