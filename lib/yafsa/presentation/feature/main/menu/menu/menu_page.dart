dcaea332cab0fdf2f9252909991fdc476e4e00a0
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-03-22 21:13:39 +0200

    feat: add menu

---
+++ lib/yafsa/presentation/feature/main/menu/menu/menu_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'menu_view.dart';
+
+class MenuPage extends StatelessWidget {
+  const MenuPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MenuView();
+  }
+}
+
