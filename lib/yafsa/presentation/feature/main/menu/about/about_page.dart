7390ec37918c8235c599daecbbdbb443449b87eb
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-03-22 21:13:48 +0200

    feat: add about

---
+++ lib/yafsa/presentation/feature/main/menu/about/about_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'about_view.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AboutView();
+  }
+}
