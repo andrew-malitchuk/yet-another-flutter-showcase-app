940f7c60178f639e8440a2c428f53a6edda83160
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-03-23 21:14:03 +0200

    feat: add preference

---
+++ lib/yafsa/presentation/feature/main/menu/preference/preference_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:yafsa/yafsa/presentation/feature/main/menu/preference/preference_view.dart';
+
+class PreferencePage extends StatelessWidget {
+  const PreferencePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferenceView();
+  }
+}
