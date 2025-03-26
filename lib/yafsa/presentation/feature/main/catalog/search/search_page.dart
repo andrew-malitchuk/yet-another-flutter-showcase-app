addb12c7b2c8bbf78c85af6d21447af703b3a845
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-03-26 21:17:10 +0200

    feat: add search

---
+++ lib/yafsa/presentation/feature/main/catalog/search/search_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:yafsa/yafsa/presentation/feature/main/catalog/search/search_view.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SearchView();
+  }
+}
