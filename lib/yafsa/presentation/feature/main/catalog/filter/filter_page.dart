9a83e21618365850865de31bcb547cf400973ced
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-03-28 21:17:47 +0200

    feat: add filter

---
+++ lib/yafsa/presentation/feature/main/catalog/filter/filter_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'filter_view.dart';
+
+class FilterPage extends StatelessWidget {
+  const FilterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FilterView();
+  }
+}
