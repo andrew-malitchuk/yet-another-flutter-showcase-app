4eec390d5e00be0602936c610bb61007eb3ee0bf
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-03-30 21:20:28 +0300

    feat: add catalog

---
+++ lib/yafsa/presentation/feature/main/catalog/catalog/catalog_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'catalog_view.dart';
+
+class CatalogPage extends StatelessWidget {
+  const CatalogPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CatalogView();
+  }
+}
