769cbe0a31a7038054d2c0ef265edb1f103d7da1
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-04-01 21:21:38 +0300

    feat: add language

---
+++ lib/yafsa/presentation/feature/language/language_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'language_view.dart';
+
+class LanguagePage extends StatelessWidget {
+  const LanguagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LanguageView();
+  }
+}
